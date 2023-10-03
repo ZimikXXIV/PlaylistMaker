f8a93e78cbbe1663107190929d2c86e9075d8d7c
Zimik <Zimik24@yandex.ru>
2023-10-03 19:34:51 +0500

    19-ый commit Исправлены замечания

--- app/src/main/java/com/example/playlistmaker/MediaLibraryActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MediaLibraryActivity.kt
@@ -1,9 +1,9 @@
 package com.example.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MedialibraryActivity : AppCompatActivity() {
+class MediaLibraryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_medialibrary)
